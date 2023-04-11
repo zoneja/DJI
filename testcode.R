1031f92e9253f3ee6cfe46e4b9487aa924ab4a34
Sonja Herrmann <116792706+zoneja@users.noreply.github.com>
2023-04-11 11:32:28 +0200

    update

--- testcode.R
+++ testcode.R
@@ -1,2 +1,9 @@
 data(mtcars)
 head(mtcars)
+gearType<-table(mtcars$gear)
+View(gearType)
+#vector of names appearing under each bar
+xVal<-c("Type 3", "Type 4","Type 5")
+# Plot the bar chart 
+barplot(gearType,names.arg=xVal,xlab="Gear Types",ylab="Total # of each type",col="green",
+        main="Distribution Of Gear Types",border="black")
