e6617cc4d9be267e7a83f932d2db65dcc9be6371
Sonja Herrmann <116792706+zoneja@users.noreply.github.com>
2023-04-11 11:50:48 +0200

    Inserted a comment

--- testcode.R
+++ testcode.R
@@ -7,3 +7,5 @@ xVal<-c("Type 3", "Type 4","Type 5")
 # Plot the bar chart 
 barplot(gearType,names.arg=xVal,xlab="Gear Types",ylab="Total # of each type",col="green",
         main="Distribution Of Gear Types",border="black")
+
+## some test code
